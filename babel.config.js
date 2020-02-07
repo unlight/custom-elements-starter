9a28982d5e7508998450f10f0678906975ff6e47
roman <s91630277@gmail.com>
2020-02-07 19:48:52 +0400

    babel/preset-env

--- babel.config.js
+++ babel.config.js
@@ -1,5 +1,16 @@
 module.exports = {
-    presets: ['@babel/preset-typescript'],
+    presets: [
+        '@babel/preset-typescript',
+        [
+            '@babel/preset-env',
+            {
+                targets: {
+                    esmodules: true,
+                },
+                loose: true,
+            },
+        ],
+    ],
     plugins: [
         ['@babel/plugin-proposal-decorators', { legacy: true }],
         ['@babel/plugin-proposal-class-properties', { loose: true }],
