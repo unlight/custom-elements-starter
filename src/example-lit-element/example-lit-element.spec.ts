6786a91fd738b1400b31cc155c19f3fb8887a3a7
roman <s91630277@gmail.com>
2020-02-12 22:20:59 +0400

    Fixed test

--- src/example-lit-element/example-lit-element.spec.ts
+++ src/example-lit-element/example-lit-element.spec.ts
@@ -1,13 +1,13 @@
-import { MyElement } from './example-lit-element';
+import { ExampleLitElement } from './example-lit-element';
 
 describe('example-lit-element', () => {
     it('smoke', () => {
-        const element = new MyElement();
+        const element = new ExampleLitElement();
         expect(element).toBeTruthy();
     });
 
     it('prop', () => {
-        const element = new MyElement();
+        const element = new ExampleLitElement();
         expect(element.foo).toBe('foo bar');
     });
 });
